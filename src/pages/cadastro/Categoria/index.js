e089b14a4fc9ebc78160c056bf545804a5f91850
Willian Lira <55150867+frontneon@users.noreply.github.com>
2020-07-28 22:40:23 -0300

    fully loaded

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
 
 function CadastroCategoria() {
   return (
     <PageDefault>
-      <h1>Cadastrar Categoria</h1>
+      <h1>Cadastro de Categoria</h1>
 
-      <Link to="/cadastro/video">
-        Cadastrar Video
+      <form>
+
+        <label>
+          Nome da Categoria:
+          <input
+            type="text"
+          />
+        </label>
+
+        <button>
+          Cadastrar
+        </button>
+      </form>
+
+
+      <Link to="/">
+        Ir para home
       </Link>
     </PageDefault>
   )
